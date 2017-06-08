331173a62859d682638e1331eb97928b13cf2e18
Holden Grissett <holdeng96@gmail.com>
2017-06-08 16:16:42 +0000

    add custom digits_pi.py

---
+++ pi/custom_digits_pi.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+
+'''
+
+Print out pi up to N number of digits
+
+Usage:
+
+./custom_digits_pi.py 15
+3.14159265358979
+
+'''
+
+from sys import argv
+from math import factorial
+from decimal import Decimal, getcontext
+
+
+def custom_digits_pi(n=10):
+    getcontext().prec=n
+    print(sum(1/Decimal(16)**k * 
+              (Decimal(4)/(8*k+1) - 
+               Decimal(2)/(8*k+4) - 
+               Decimal(1)/(8*k+5) -
+               Decimal(1)/(8*k+6)) for k in range(100)))
+
+if __name__ == "__main__":
+    custom_digits_pi(int(argv[1]))
