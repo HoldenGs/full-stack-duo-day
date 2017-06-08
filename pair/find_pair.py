97a02b909fe1f51a537f235c34b2ba9f6a2eaeaa
Holden Grissett <holdeng96@gmail.com>
2017-06-08 17:44:06 +0000

    add find_pair.py

---
+++ pair/find_pair.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+'''
+
+Find the closest pairs of points in an array of 2D or 3D points
+
+Usage:
+./find_pair.py
+
+'''
+
+
+def find_pair(array=[]):
+    dimensions = 2
+    if len(array) == 0:
+        return "No array found"
+    if len(array[0]) == 3:
+        dimensions = 3
+    distance = ((array[0][0] - array[1][0])**2 +
+                (array[0][1] - array[1][1])**2)**0.5
+    closest_pair = ((0, 1), distance)
+    for i1, point_1 in enumerate(array):
+        for i2, point_2 in enumerate(array):
+            new_distance = ((point_1[0] - point_2[0])**2 +
+                        (point_1[1] - point_2[1])**2)**0.5
+            if new_distance < closest_pair[1] and i1 != i2:
+                closest_pair = ((i1, i2), new_distance)
+    return closest_pair
+
+
+array = [(1, 5), (1, 7), (1, 8), (1, 2)]
+
+new_array = [(3, 5), (4, -1), (7, 2), (1, 8), (8, 9)]
+
+print(find_pair(new_array))
